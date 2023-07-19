c59168abf7d29f699cb1bf021ed60456c37306ca
joojjang <itchu999@naver.com>
2023-07-19 19:31:35 +0900

    login-page update

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -5,16 +5,27 @@ function App() {
   return (
     <div className="App">
       <form>
-        <h4 id="title">Log In</h4>
+        <strong id="title">Log In</strong>
         <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용하세요!</div>
-        
+        <br/><br/>
 
-        <div>
-          <label className="txt">ID </label><input type="text"></input>
-          <label className="txt">PW </label><input type="password"></input>
+        <div >
+          <table>
+            <tr>
+              <th><label className="txt">ID</label></th>
+              <th><input type="text"></input></th>
+            </tr>
+
+            <tr>
+              <th><label className="txt">PW</label></th>
+              <th><input type="password"></input></th>
+            </tr>
+          </table>
+          <br/>
+          
+          <button className="btn">로그인</button>
         </div>
 
-        <button className="btn">로그인</button>
       </form>
       
     </div>
