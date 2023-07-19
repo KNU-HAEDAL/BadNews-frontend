867d118783f9b73ad62e756e1411cf7c6d3e3a79
joojjang <itchu999@naver.com>
2023-07-20 01:44:16 +0900

    login-page update
    package.json 파일 이슈 해결...

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <form>
-                <div id="login">Log In</div>
-                로그인 후 Bad News를 더욱 편리하게 사용하세요!
-                <div>
-                    <label>ID</label>
-                    <input type="text"></input>
-                    <label>ID</label>
-                    <input type="text"></input>
-                </div>
-                <button className="btn">로그인</button>
-            </form>
-        </div>
-    );
-=======
-import logo from './logo.svg';
-import './App.css';
+function LoginBlock() {
+  return <div>
+    <strong id="title">Log In</strong>
+    <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용하세요!</div>
+  </div>
+}
 
 function Table() {
   return <table>
@@ -42,19 +28,15 @@ function App() {
   return (
     <div className="App">
       <form>
-        <strong id="title">Log In</strong>
-        <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용하세요!</div>
+        <LoginBlock/>
         <br/>
-
         <Table/>
         <br/>
         <LoginBtn/>
-
       </form>
       
     </div>
   );
->>>>>>> c59168abf7d29f699cb1bf021ed60456c37306ca
 }
 
 export default App;
