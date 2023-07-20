860d74b0a51fdffecb57453e6d1a1c6dee05a423
joojjang <itchu999@naver.com>
2023-07-20 12:23:59 +0900

    Update login-page

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
-function TextBlock() {
+const TextBlock = () => {
   return <div>
     <strong className="title">Log In</strong>
     <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용하세요!</div>
   </div>
 }
 
-function Table() {
+const Table = () => {
   return <div className="input-table-outer">
     <table className="input-table">
       <tr>
@@ -22,7 +22,7 @@ function Table() {
   </div>
 }
 
-function LoginBtn() {
+const LoginBtn = () => {
   return <div className="login-btn-outer">
     <button className="login-btn">로그인</button>
     </div>
