4c2b2314538179f84c128997067a87aac903d6e9
joojjang <itchu999@naver.com>
2023-07-21 03:36:59 +0900

    Update MainPage.js

--- news-pages/src/pages/MainPage.js
+++ news-pages/src/pages/MainPage.js
@@ -1,5 +1,4 @@
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import Sidebar from '../components/Sidebar';
 import CategoriesResult from './components/Contents/CategoriesResult';
 import { Link } from 'react-router-dom';
 
