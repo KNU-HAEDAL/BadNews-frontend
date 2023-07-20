89554a506086e5be3a104d617c708f87b03325c3
joojjang <itchu999@naver.com>
2023-07-21 01:59:05 +0900

    Update App.js

--- news-pages/src/pages/MainPages.js
+++ news-pages/src/pages/MainPages.js
@@ -1,1 +1,18 @@
 import Header from '../components/Header';
+import Categories from '../components/Categories';
+import CategoriesResult from '../components/Contents/CategoriesResult';
+import { Link } from 'react-router-dom';
+
+const MainPages = () => {
+    return (
+        <div className="MainPages">
+            <form>
+                <Header />
+                <Categories />
+                <CategoriesResult />
+            </form>
+        </div>
+      );
+}
+
+export default MainPages;
