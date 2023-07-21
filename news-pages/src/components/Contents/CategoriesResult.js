f2ce7419478b35876c5b6c6aba934194c80ac88d
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-07-22 06:52:55 +0900

    카테고리쪽 수정중임요..

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -6,12 +6,14 @@ import './CategoriesResult.css';
 const title = '제목입니다';
 const date = '2023.07.18';
 const summary = 'GPT가 세상을 지배하고 있다.';
+const categoryName = '정치';
 
 const CategoriesResult = () => {
     // let match = useMatch(); /* 현재 경로의 path */
 
     return (
         <div>
+            <div className="CategoryNameContainer">"{categoryName}"</div>
             <div className="PageWrap1">
                 <form className="FormContainer">
                     <h2 className="TitleItem">{title}</h2>
@@ -19,14 +21,14 @@ const CategoriesResult = () => {
                     <ul className="SummaryItem">{summary}</ul>
                 </form>
             </div>
-            <div className="PageWrap1">
+            <div className="PageWrap2">
                 <form className="FormContainer">
                     <h2 className="TitleItem">{title}</h2>
                     <ul className="DateItem"> {date}</ul>
                     <ul className="SummaryItem">{summary}</ul>
                 </form>
             </div>
-            <div className="PageWrap1">
+            <div className="PageWrap3">
                 <form className="FormContainer">
                     <h2 className="TitleItem">{title}</h2>
                     <ul className="DateItem"> {date}</ul>
