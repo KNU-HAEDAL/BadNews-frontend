7fc8f68ceeac4bd64631bd36c72dd8e2c21ec2d2
joojjang <itchu999@naver.com>
2023-07-22 02:52:25 +0900

    Update Sidebar.css

--- news-pages/src/components/SideBar.js
+++ news-pages/src/components/SideBar.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
+import category from '../category.png'
 
 const SideBar = () => {
     return (
         <form className="Area">
-            <div id="caterogies-title"><strong>카테고리</strong></div>
+            <div className="categories-container">
+                <div id="caterogies-title">
+                    <img src={category} alt="caregories icon" id="categories-icon"/>
+                    <strong>카테고리</strong>
+                </div>
 
-            <button className="categories-btn">정치</button>
-            <button className="categories-btn">경제</button>
-            <button className="categories-btn">사회</button>
-            <button className="categories-btn">IT | 테크</button>
-            <button className="categories-btn">연예</button>
-            <button className="categories-btn">스포츠 | 예술</button>
+                <button className="categories-btn" onClick="">정치</button>
+                <button className="categories-btn">경제</button>
+                <button className="categories-btn">사회</button>
+                <button className="categories-btn">IT | 테크</button>
+                <button className="categories-btn">연예</button>
+                <button className="categories-btn">스포츠 | 예술</button>
+            </div>
         </form>
     );
 };
