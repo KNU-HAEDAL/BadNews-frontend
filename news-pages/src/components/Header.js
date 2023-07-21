9f08d142f38eccf5704a03f46f1985cc5226bbfe
joojjang <itchu999@naver.com>
2023-07-21 17:14:46 +0900

    Update Header.js & Header.css

--- news-pages/src/components/Header.js
+++ news-pages/src/components/Header.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './Header.SCSS';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    const style = {
-        container: {
-            marginLeft: '15px',
-        },
-        title: {
-            color: '#0357FF',
-        },
-    };
+const style = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        height: '50px',
+    },
+    title: {
+        color: '#0357FF',
+        fontSize: '35px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textDecoration: 'none',
+        paddingLeft: '25px'
+    },
+};
 
+const Header = () => {
     return (
         <div style={style.container}>
-            <Link to="/" style={{textDecoration: 'none'}}><h1 style={style.title}>Bad News</h1></Link>
+            <Link to="/" style={style.title}><strong>Bad News</strong></Link>
         </div>
     );
 };
