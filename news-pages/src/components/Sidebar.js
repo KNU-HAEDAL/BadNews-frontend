15d6b527c43cc79de02269ccc571d4f91c0f3a67
joojjang <itchu999@naver.com>
2023-07-22 02:34:30 +0900

    Update Header.css, Sidebar.css

--- news-pages/src/components/Sidebar.js
+++ news-pages/src/components/Sidebar.js
@@ -4,16 +4,13 @@ import './Sidebar.css';
 const SideBar = () => {
     return (
         <form className="Area">
+            <div id="caterogies-title"><strong>카테고리</strong></div>
+
             <button className="categories-btn">정치</button>
-
             <button className="categories-btn">경제</button>
-
             <button className="categories-btn">사회</button>
-
             <button className="categories-btn">IT | 테크</button>
-
             <button className="categories-btn">연예</button>
-
             <button className="categories-btn">스포츠 | 예술</button>
         </form>
     );
