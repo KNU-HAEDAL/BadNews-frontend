0e1537a82ef8a9bdf07a9b25303b9a10d1b9ff98
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-07-21 14:54:09 +0900

    수정중

--- news-pages/src/pages/MainPage.js
+++ news-pages/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import Sidebar from '../components/Sidebar';
 import CategoriesResult from '../components/Contents/CategoriesResult';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 const MainPage = () => {
     return (
