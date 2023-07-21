27f84a655da1fa7d64dff65310f07160ed220af4
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-07-22 03:20:06 +0900

    sidebar 수정

--- news-pages/src/components/SideBar.js
+++ news-pages/src/components/SideBar.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
-import category from '../category.png'
+import category from '../category.png';
 
 const SideBar = () => {
     return (
         <form className="Area">
             <div className="categories-container">
                 <div id="caterogies-title">
-                    <img src={category} alt="caregories icon" id="categories-icon"/>
+                    <img src={category} alt="caregories icon" id="categories-icon" />
                     <strong>카테고리</strong>
                 </div>
 
-                <button className="categories-btn" onClick="">정치</button>
+                <button className="categories-btn" onClick="">
+                    정치
+                </button>
                 <button className="categories-btn">경제</button>
                 <button className="categories-btn">사회</button>
                 <button className="categories-btn">IT | 테크</button>
