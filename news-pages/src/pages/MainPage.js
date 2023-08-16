39f787c867e5cca96a328ec8b7fe4418d3eea42b
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-08-16 18:49:36 +0900

    임시저장

--- news-pages/src/pages/MainPage.js
+++ news-pages/src/pages/MainPage.js
@@ -2,12 +2,13 @@ import Sidebar from '../components/Sidebar';
 import CategoriesResult from '../components/Contents/CategoriesResult';
 import SearchResult from '../components/Contents/SearchResult';
 import { Route, Routes, Link, useLocation } from 'react-router-dom';
-
 const MainPage = () => {
     return (
-        <div className="MainPage" style={{ height: 'calc(100% - 101px)'}}>
+        <div className="MainPage" style={{ height: 'calc(100% - 101px)' }}>
             <Sidebar />
-
+            <div style={{ float: 'left' }}>
+                <CategoriesResult />
+            </div>
             <Routes>
                 <Route exact path="/" element={<CategoriesResult />} />
                 <Route exact path="/search-result" element={<SearchResult />} />
