313e338bb989931388bd1c1c1e402c246f3195db
joojjang <itchu999@naver.com>
2023-08-16 15:04:27 +0900

    Sidebar-민주 <- main (#8)
    * Update Sidebar.css
    
    * 카테로그결과 수정중
    
    * UI 디자인 살짝 수정
    
    * form태그 div로 변경
    
    * 카테고리쪽 수정중임요..
    
    * "{카테고리이름}" 나오게 할려다가 제거함
    
    * 이것저것 수정중...
    
    * change head title & update README file
    
    * Add favicon
    
    ---------
    
    Co-authored-by: Dobbymin <79429482+Dobbymin@users.noreply.github.com>

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -1,9 +1,42 @@
 import './PageNumbers';
 import PageNumbers from './PageNumbers';
+import React from 'react';
+import './CategoriesResult.css';
+
+const title = '제목입니다';
+const date = '2023.07.18';
+const summary = 'GPT가 세상을 지배하고 있다.';
+const categoryName = '정치';
+
 const CategoriesResult = () => {
     // let match = useMatch(); /* 현재 경로의 path */
 
-    return <div className="CategoriesResult"></div>;
+    return (
+        <div>
+            {/* <div className="CategoryNameContainer">"{categoryName}"</div> */}
+            <div className="PageWrap1">
+                <form className="FormContainer">
+                    <h2 className="TitleItem">{title}</h2>
+                    <ul className="DateItem"> {date}</ul>
+                    <ul className="SummaryItem">{summary}</ul>
+                </form>
+            </div>
+            <div className="PageWrap2">
+                <form className="FormContainer">
+                    <h2 className="TitleItem">{title}</h2>
+                    <ul className="DateItem"> {date}</ul>
+                    <ul className="SummaryItem">{summary}</ul>
+                </form>
+            </div>
+            <div className="PageWrap3">
+                <form className="FormContainer">
+                    <h2 className="TitleItem">{title}</h2>
+                    <ul className="DateItem"> {date}</ul>
+                    <ul className="SummaryItem">{summary}</ul>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default CategoriesResult;
