70bae125766eaf7d5cb8fb3b8a521660baa1b9c6
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2023-08-21 16:50:32 +0900

    카테고리명 변경 (#3)

--- news-pages/src/components/Sidebar.js
+++ news-pages/src/components/Sidebar.js
@@ -23,7 +23,7 @@ const Sidebar = () => {
         };
 
         axios
-            .post('http://localhost:8080/article/save', {
+            .post('http://13.124.161.27:8080/article/save', {
                 userId: idValue,
                 category: ctgrValue,
                 sort: 1,
@@ -78,19 +78,19 @@ const Sidebar = () => {
                 <button
                     className="categories-btn"
                     onClick={() => {
-                        showArticles('IT/테크');
+                        showArticles('IT/과학');
                     }}
                 >
-                    IT | 테크
+                    IT | 과학
                 </button>
 
                 <button
                     className="categories-btn"
                     onClick={() => {
-                        showArticles('문화/예술');
+                        showArticles('생활/문화');
                     }}
                 >
-                    문화 | 예술
+                    생활 | 문화
                 </button>
 
                 <button
@@ -99,7 +99,7 @@ const Sidebar = () => {
                         showArticles('스포츠');
                     }}
                 >
-                    스포츠
+                    스포츠(사용x)
                 </button>
             </div>
         </div>
