d60f49d86bd0c48b7e994de4b2f3cd337226508e
Dobbymin <gmin0701@knu.ac.kr>
2023-08-23 03:09:01 +0900

    페이지 번호 (자고 일어나서 적용해볼게요....)

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, Link } from 'react';
 import './CategoriesResult.css';
 import './PageNumbers';
 import PageNumbers from './PageNumbers';
-import unmarked from '../../bookmark_unmarked.png'
-import marked from '../../bookmark_marked.png'
-
+import unmarked from '../../bookmark_unmarked.png';
+import marked from '../../bookmark_marked.png';
 
 const title1 = '상온 초전도체??? 대박일까?';
 const title2 = '태풍이 하나 더 올까?';
@@ -15,12 +14,11 @@ const summary2 = '기상청의 잇따른 정보 오류로 인해... 국민들 �
 const summary3 = '그럼 진짜 사람이 필요 없지 않을까...? 전세계 떠들썩..';
 const categoryName = '정치';
 
-
 const CategoriesResult = () => {
     // let match = useMatch(); /* 현재 경로의 path */
 
     const [isMarked, setIsMarked] = useState(false);
-    
+
     const handleBookmarkClick = () => {
         // 북마크 상태 핸들러
         // 마크 상태이면 setIsMarked(isMarked = true)
@@ -34,9 +32,10 @@ const CategoriesResult = () => {
             {/* <div className="CategoryNameContainer">"{categoryName}"</div> */}
             <div className="PageWrap1">
                 <form className="FormContainer">
-                    <img src={unmarked} alt="bookmark_unmarked" id="unmarked"/>
+                    <img src={unmarked} alt="bookmark_unmarked" id="unmarked" />
                     <Link to="/login" className="header-menu" onClick={handleBookmarkClick}>
-                    {isMarked ? '로그아웃' : '로그인'}</Link>
+                        {isMarked ? '로그아웃' : '로그인'}
+                    </Link>
                     <h2 className="TitleItem">{title1}</h2>
                     <ul className="DateItem"> {date}</ul>
                     <ul className="SummaryItem">{summary1}</ul>
