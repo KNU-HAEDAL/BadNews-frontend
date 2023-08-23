1e40521292de07c990566831e1aa729eb405528f
joojjang <itchu999@naver.com>
2023-08-24 03:45:56 +0900

    Login&Signup/Header-민주
    commit d67748e8ec5a432b4395fe5bd6442067fb00b488
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 03:25:04 2023 +0900
    
        Update Header.js
    
        로그인 상태 값 받아오는 부분 수정
    
    commit 16854663f0228d6aa96f910393a9df3bc65318ff
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 02:01:48 2023 +0900
    
        Update Signup.js
    
        Login.js와 같은 형식으로 수정
        css는 Login.css 갖다 씀
    
    commit b45bf4bf34c4de8221ddbc035aef681605b5150b
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 01:48:46 2023 +0900
    
        css 약간 수정
    
    commit b8333eeeba533ef4fd650b00e0108c15b7434ace
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 01:23:34 2023 +0900
    
        로그인 서버 통신 완료..(아마도), 회원가입으로 이동하는 링크 추가, Login.css 수정
    
    commit 361aa9eb4433d8d73876390ca8de0e060867d78b
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 00:49:27 2023 +0900
    
        Update Login.js
    
        axios도 fetch도 HTTP 요청하는 함수였던 건데... 같은 건줄 몰랐던 것이다...... feature#24 브랜치의 Signup.js랑 거의 똑같게 만들었다.
    
    commit bb581c865c9b750c9b7dd93cead22a91d710fb4b
    Author: joojjang <itchu999@naver.com>
    Date:   Wed Aug 23 23:27:25 2023 +0900
    
        Update Login.js

--- news-pages/src/components/Header.js
+++ news-pages/src/components/Header.js
@@ -1,74 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { Login } from '../pages/Login';
+import { Login } from '../pages/Login'
 import './Header.css';
-import logo from '../logo.png';
-import search from '../search.png';
+import logo from '../logo.png'
+import search from '../search.png'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import axios from 'axios'
 
 const SearchBox = () => {
     return (
         <div className="search-container">
             <input id="search-box" type="text" placeholder="검색어를 입력하세요" img></input>
-            <Link to="/">
-                <img src={search} alt="돋보기" id="search-icon" />
-            </Link>
+            <Link to="/"><img src={search} alt="돋보기" id="search-icon" /></Link>
         </div>
     );
-};
+}
 
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    
     useEffect(() => {
-        // 서버에 로그인 상태를 확인하는 요청을 보냅니다.
-        axios
-            .get('http://localhost:8080/checkLoginStatus')
-            .then((response) => {
-                // 서버에서 로그인 상태를 확인한 후, 그 결과를 상태로 설정합니다.
-                setIsLoggedIn(response.data.isLoggedIn);
+        // 서버에 로그인 상태 확인 요청 전송
+        axios.get('http://13.124.161.27:8080/login')
+            .then(response => {
+                // 서버에서 로그인 상태를 확인한 후, 그 결과를 상태로 설정
+                if (response.data.result) { // 로그인 상태
+                    setIsLoggedIn(true);
+                } else { // 로그아웃 상태
+                    setIsLoggedIn(false);
+                }
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error('Error:', error);
             });
     }, []);
 
     const handleLogin = () => {
-        // if return(idValue) == true:
         setIsLoggedIn(true);
-    };
+    }
 
     const handleLogout = () => {
         setIsLoggedIn(false);
-    };
+    }
 
-    return (
-        <div className="Header">
-            <Link to="/" className="title-container">
-                <img src={logo} alt="Bad News logo" id="logo" />
-                <strong className="header-title">Bad News</strong>
-            </Link>
+    return ( 
+    <div className="Header">
+        <Link to="/" className="title-container">
+            <img src={logo} alt="Bad News logo" id="logo"/>
+            <strong className="header-title">Bad News</strong>
+        </Link>
 
-            <div className="search-login-container">
-                <SearchBox />
+        <div className="search-login-container">
+            <SearchBox />
 
-                <div className="header-menu-container">
-                    {isLoggedIn ? (
-                        <Link to="/login" className="header-menu" onClick={handleLogout}>
-                            로그인
-                        </Link>
-                    ) : (
-                        <Link to="/" className="header-menu" onClick={handleLogin}>
-                            로그아웃
-                        </Link>
-                    )}
-                    <div className="header-menu-bar">|&nbsp;</div>
-                    <Link to="/mypage" className="header-menu">
-                        마이페이지
-                    </Link>
-                </div>
+            <div className="header-menu-container">
+                { isLoggedIn ? 
+                    ( <Link to="/login" className="header-menu" onClick={useEffect}>로그인</Link> ) : 
+                    ( <Link to="/" className="header-menu" onClick={useEffect}>로그아웃</Link> ) 
+                } 
+                <div className="header-menu-bar">|&nbsp;</div> 
+                <Link to="/mypage" className="header-menu">마이페이지</Link>
             </div>
         </div>
+    </div>
     );
 };
 
