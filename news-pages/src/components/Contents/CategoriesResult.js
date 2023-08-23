8070ae771cd70865ed8aa0386a611ae8f0d18758
Dobbymin <gmin0701@knu.ac.kr>
2023-08-23 14:20:35 +0900

    수정중인것들

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -1,17 +1,15 @@
 import React, { useState, Link } from 'react';
 import './CategoriesResult.css';
-import './PageNumbers';
-import PageNumbers from './PageNumbers';
 import unmarked from '../../bookmark_unmarked.png';
 import marked from '../../bookmark_marked.png';
+import Posts from './Posts';
+import { NavLink } from 'react-router-dom';
 
 const title1 = '상온 초전도체??? 대박일까?';
 const title2 = '태풍이 하나 더 올까?';
-const title3 = 'GPT 5.0이 등장할까???';
 const date = '2023.07.18';
 const summary1 = '상온 초전도체 연구가 성공만 한다면... 우리나라는 부자가 될수있겠지?';
 const summary2 = '기상청의 잇따른 정보 오류로 인해... 국민들 모두 "혼란"';
-const summary3 = '그럼 진짜 사람이 필요 없지 않을까...? 전세계 떠들썩..';
 const categoryName = '정치';
 
 const CategoriesResult = () => {
@@ -48,6 +46,8 @@ const CategoriesResult = () => {
                     <ul className="SummaryItem">{summary2}</ul>
                 </form>
             </div>
+            {/* 페이지 하단에 번호 */}
+            <Posts />
         </div>
     );
 };
