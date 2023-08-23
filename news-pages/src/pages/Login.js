af0dd6b970a4baf225642b8edfc3fa05d7d00ecd
joojjang <itchu999@naver.com>
2023-08-24 01:28:17 +0900

    로그인 통신... 그리고 회원가입 링크 추가 및 로그인 css 수정
    commit b8333eeeba533ef4fd650b00e0108c15b7434ace
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 01:23:34 2023 +0900
    
        로그인 서버 통신 완료..(아마도), 회원가입으로 이동하는 링크 추가, Login.css 수정
    
    commit 361aa9eb4433d8d73876390ca8de0e060867d78b
    Author: joojjang <itchu999@naver.com>
    Date:   Thu Aug 24 00:49:27 2023 +0900
    
        Update Login.js
    
        axios도 fetch도 HTTP 요청하는 함수였던 건데... 같은 건줄 몰랐던 것이다...... feature#24 브랜치의 Signup.js랑 거의 똑같게 만들었다.
    
    commit bb581c865c9b750c9b7dd93cead22a91d710fb4b
    Author: joojjang <itchu999@naver.com>
    Date:   Wed Aug 23 23:27:25 2023 +0900
    
        Update Login.js

--- news-pages/src/pages/Login.js
+++ news-pages/src/pages/Login.js
@@ -2,14 +2,16 @@ import './Login.css';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+
 const TextBlock = () => {
     return (
-        <div style={{ paddingLeft: '5px' }}>
+        <div style={{paddingLeft: '5px'}}>
             <strong className="login-title">Log In</strong>
             <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용해보세요!</div>
         </div>
     );
 };
+
 
 const InputTable = () => {
     return (
@@ -32,69 +34,77 @@ const InputTable = () => {
     );
 };
 
+
 const LoginBtn = () => {
     const navigate = useNavigate();
 
-    const saveAndSend = () => {
-        //event.preventDefault();
-
+    const saveAndSend = () => {   
         // 변수에 인풋값 저장
         const idValue = document.getElementById('id').value;
         const pwValue = document.getElementById('pw').value;
-        const data = {
-            id: idValue,
-            pw: pwValue,
-        };
+        // const data = {
+        //     id: idValue,
+        //     pw: pwValue,
+        // };
 
-        // 서버로 회원가입 요청
+        // 서버로 로그인 요청
         axios
-            .post('http://localhost:8080/signup', {
+            .post('http://13.124.161.27:8080/login', {
                 id: idValue,
                 password: pwValue,
             })
             //성공 시 then 실행
             .then(function (response) {
+                if (response.data.result) {
+                    console.log(`${response.data.id}님 환영합니다`);
+                    navigate("/");
+                } else {
+                    console.log(`로그인 실패. 다시 시도하세요.`)
+                }
                 console.log(response.data);
+                navigate("/"); // root로 이동
             })
             //실패 시 catch 실행
             .catch(function (error) {
-                console.log('Error: 회원 가입 실패. 다시 시도하세요. ', error);
+                console.log(`다시 시도하세요.`);
             });
-
+    
         // 서버로 로그인 요청
-        fetch('http://localhost:8080/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result == true) {
-                    alert(`${idValue}님 환영합니다`);
-                    navigate('/'); // root로 이동
-                } else {
-                    alert('로그인 실패');
-                }
-            })
-            .catch((error) => {
-                console.error('Error: User 정보가 없습니다. ', error);
-            });
-
-        // alert(`${idValue}님 환영합니다`);
-        // navigate("/"); // root로 이동
-    };
+        // fetch('http://13.124.161.27:8080/login', 
+        //     { method: 'POST',
+        //     headers: { 'Content-Type': 'application/json', },
+        //     body: JSON.stringify( { id: idValue, password: pwValue } ), }
+        // )
+        //     .then((response) => response.json()) // 서버로부터의 응답을 파싱하고, data라는 변수에 담음
+        //     .then((data) => {
+        //         // console.log(data); // 응답 출력. 확인용
+        //         if (data.result) {
+        //             alert(`${data.id}님 환영합니다`);
+        //             navigate("/"); // root로 이동
+        //         } 
+        //         else {
+        //             alert(`다시 시도하세요`);
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         console.error('Error: User 정보가 없습니다. ', error);
+        //     });
+    }
 
     return (
-        <div className="login-btn-outer">
-            <button className="login-btn" onClick={saveAndSend}>
-                로그인
-                {/* <Link to="/" className="styled-link">로그인</Link> */}
-            </button>
+        <div className="login-btn-container">
+             <table className="login-btn-table">
+                <tr>
+                    <button className="login-btn" onClick={saveAndSend}>로그인</button>
+                </tr>
+                <tr>
+                    <div className="small-txt">아직 계정이 없으신가요? <Link to="/signup">회원가입</Link></div>
+                </tr>
+            </table>
         </div>
     );
 };
+
 
 const Login = () => {
     return (
