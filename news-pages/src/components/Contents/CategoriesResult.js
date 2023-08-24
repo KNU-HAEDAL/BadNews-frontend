2e419122fb689f8e6c3ae89cf5300c8e865bea61
Dobbymin <gmin0701@knu.ac.kr>
2023-08-25 01:22:41 +0900

    [FIX] 로그인 | 페이지번호 수정중

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -1,87 +1,95 @@
-import React, { useState, useEffect } from "react";
-import "./CategoriesResult.css";
-// import "./PageNumbers";
-// import PageNumbers from './PageNumbers';
-import unmarked from "../../bookmark_unmarked.png";
-import marked from "../../bookmark_marked.png";
-
-const title1 = "상온 초전도체??? 대박일까?";
-const title2 = "태풍이 하나 더 올까?";
-const summary1 =
-  "상온 초전도체 연구가 성공만 한다면... 우리나라는 부자가 될수있겠지?";
-const summary2 = '기상청의 잇따른 정보 오류로 인해... 국민들 모두 "혼란"';
+import React, { useState, useEffect } from 'react';
+import './CategoriesResult.css';
+import Posts from './Posts';
+import unmarked from '../../bookmark_unmarked.png';
+import marked from '../../bookmark_marked.png';
 
 const CategoriesResult = () => {
-  const [isMarked, setIsMarked] = useState(false);
+    const [isMarked, setIsMarked] = useState(false);
 
-  const handleBookmarkClick = () => {
-    var image = document.getElementById("bookmark");
-    image.src = marked;
-  };
-
-  useEffect(() => {
-    const observerCallback = (entries) => {
-      entries.forEach((entry) => {
-        // 각 entry에 대한 처리 로직 작성
-        if (entry.isIntersecting) {
-          console.log("Form element is intersecting!");
-          // 관찰되었을 때 실행할 동작 추가
-        }
-      });
+    const handleBookmarkClick1 = () => {
+        var image = document.getElementById('bookmark');
+        image.src = marked;
     };
 
-    const observerOptions = {
-      root: document.querySelector(".ScrollableSection"), // 스크롤바를 적용할 부모 요소 선택
-      rootMargin: "0px",
-      threshold: 0.5,
+    const handleBookmarkClick2 = () => {
+        var image = document.getElementById('bookmark');
+        image.src = marked;
     };
 
-    const observer = new IntersectionObserver(
-      observerCallback,
-      observerOptions
+    useEffect(() => {
+        const observerCallback = (entries) => {
+            entries.forEach((entry) => {
+                // 각 entry에 대한 처리 로직 작성
+                if (entry.isIntersecting) {
+                    console.log('Form element is intersecting!');
+                    // 관찰되었을 때 실행할 동작 추가
+                }
+            });
+        };
+
+        const observerOptions = {
+            root: document.querySelector('.ScrollableSection'), // 스크롤바를 적용할 부모 요소 선택
+            rootMargin: '0px',
+            threshold: 0.5,
+        };
+
+        const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+        const formsToObserve = document.querySelectorAll('.ScrollableSection form');
+
+        formsToObserve.forEach((form) => {
+            observer.observe(form);
+        });
+
+        return () => {
+            formsToObserve.forEach((form) => {
+                observer.unobserve(form);
+            });
+
+            // Observer 해제 로직 추가 (컴포넌트 언마운트 시)
+        };
+    }, []); // 빈 배열은 컴포넌트가 처음 마운트될 때만 실행
+
+    return (
+        <div className="CategoriesResult">
+            {/*<div className="CategoryName">"{categoryName}"</div>*/}
+            <div className="today-text">오늘의 기사 요약!</div>
+
+            <div className="article-container">
+                <div className="bookmark-container">
+                    <img src={unmarked} alt="unmarked" id="bookmark" onClick={handleBookmarkClick1} />
+                </div>
+                <strong className="article-title">상온 초전도체??? 대박일까?</strong>
+                <div className="article-date">2023-08-23</div>
+                <div className="article-summary">
+                    상온 초전도체 연구가 성공만 한다면... 우리나라는 부자가 될 수 있겠지?
+                </div>
+            </div>
+
+            <div className="article-container">
+                <div className="bookmark-container">
+                    <img src={unmarked} alt="unmarked" id="bookmark" onClick={handleBookmarkClick1} />
+                </div>
+                <strong className="article-title">태풍이 하나 더 올까?</strong>
+                <div className="article-date">2023-08-23</div>
+                <div className="article-summary">기상청의 잇따른 정보 오류로 인해... 국민들 모두 "혼란"</div>
+            </div>
+
+            <div className="article-container">
+                <div className="bookmark-container">
+                    <img src={unmarked} alt="unmarked" id="bookmark" onClick={handleBookmarkClick1} />
+                </div>
+                <strong className="article-title">그냥 하나 더 추가</strong>
+                <div className="article-date">2023-08-23</div>
+                <div className="article-summary">룰룰........즐겁다</div>
+            </div>
+
+            <div className="page-numbers">
+                <Posts />
+            </div>
+        </div>
     );
-
-    const formsToObserve = document.querySelectorAll(".ScrollableSection form");
-
-    formsToObserve.forEach((form) => {
-      observer.observe(form);
-    });
-
-    return () => {
-      formsToObserve.forEach((form) => {
-        observer.unobserve(form);
-      });
-
-      // Observer 해제 로직 추가 (컴포넌트 언마운트 시)
-    };
-  }, []); // 빈 배열은 컴포넌트가 처음 마운트될 때만 실행
-
-  return (
-    <div className="ScrollBar">
-      {/*<div className="CategoryName">"{categoryName}"</div>*/}
-      {/* <div className="CategoryNameContainer">"{categoryName}"</div> */}
-      <div className="PageWrap1">
-        <form className="article-container">
-          <img
-            src={unmarked}
-            alt="unmarked"
-            id="bookmark"
-            onClick={handleBookmarkClick}
-          />
-          <h2 className="TitleItem">{title1}</h2>
-          <ul className="DateItem"> 2023-08-23 17:10:40</ul>
-          <ul className="SummaryItem">{summary1}</ul>
-        </form>
-      </div>
-      <div className="PageWrap2">
-        <form className="article-container">
-          <h2 className="TitleItem">{title2}</h2>
-          <ul className="DateItem"> 2023-08-23 17:10:40</ul>
-          <ul className="SummaryItem">{summary2}</ul>
-        </form>
-      </div>
-    </div>
-  );
 };
 
 export default CategoriesResult;
--- news-pages/src/pages/Login.js
+++ news-pages/src/pages/Login.js
@@ -1,125 +1,109 @@
-import "./Login.css";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { useState } from "react";
+import './Login.css';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 const InputTable = ({ handleIdChange, handlePwChange }) => {
-  const [idValue, setIdValue] = useState("");
-  const [pwValue, setPwValue] = useState("");
+    const [idValue, setIdValue] = useState('');
+    const [pwValue, setPwValue] = useState('');
 
-  const handleLogin = () => {
-    const datas = {
-      id: idValue,
-      pw: pwValue,
+    const handleLogin = () => {
+        const datas = {
+            id: idValue,
+            pw: pwValue,
+        };
     };
 
-    const handleIdChange = (event) => {
-      setIdValue(event.target.value);
-    };
-
-    const handlePwChange = (event) => {
-      setPwValue(event.target.value);
-    };
-  };
-  
-  return (
-    <div className="input-table-container">
-      <table className="input-table">
-        <tr>
-          {/* <th><label className="txt">ID</label></th> */}
-          <td>
-            <input
-              id="id"
-              type="text"
-              placeholder="User name"
-              onChange={handleIdChange}
-            ></input>
-          </td>
-        </tr>
-        <tr>
-          {/* <th><label className="txt">PW</label></th> */}
-          <td>
-            <input
-              id="pw"
-              type="password"
-              placeholder="Password"
-              onChange={handlePwChange}
-            ></input>
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
+    return (
+        <div className="input-table-container">
+            <table className="input-table">
+                <tr>
+                    {/* <th><label className="txt">ID</label></th> */}
+                    <td>
+                        <input id="id" type="text" placeholder="User name" onChange={handleIdChange}></input>
+                    </td>
+                </tr>
+                <tr>
+                    {/* <th><label className="txt">PW</label></th> */}
+                    <td>
+                        <input id="pw" type="password" placeholder="Password" onChange={handlePwChange}></input>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    );
 };
 
 const TextBlock = () => {
-  return (
-    <div style={{ paddingLeft: "5px" }}>
-      <strong className="login-title">Log In</strong>
-      <div className="txt">
-        로그인 후 Bad News를 더욱 편리하게 사용해보세요!
-      </div>
-    </div>
-  );
+    return (
+        <div style={{ paddingLeft: '5px' }}>
+            <strong className="login-title">Log In</strong>
+            <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용해보세요!</div>
+        </div>
+    );
 };
 
 const LoginBtn = ({ idValue, pwValue }) => {
-  const handleLogin = () => {
-    const datas = {
-      id: idValue,
-      pw: pwValue,
+    const handleLogin = () => {
+        const datas = {
+            id: idValue,
+            pw: pwValue,
+        };
+
+        axios
+            .post('http://13.124.161.27:8080/login', datas, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => response.data)
+            .then((result) => {
+                if (result === true) {
+                    alert(`${idValue}님 환영합니다`);
+                    // 여기에서 로그인 성공 시의 동작을 정의합니다.
+                } else {
+                    alert('로그인 실패');
+                }
+            })
+            .catch((error) => {
+                console.error('Error: User 정보가 없습니다. ', error);
+            });
     };
 
-    axios
-      .post("http://13.124.161.27:8080/login", datas, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => response.data)
-      .then((result) => {
-        if (result === true) {
-          alert(`${idValue}님 환영합니다`);
-          // 여기에서 로그인 성공 시의 동작을 정의합니다.
-        } else {
-          alert("로그인 실패");
-        }
-      })
-      .catch((error) => {
-        console.error("Error: User 정보가 없습니다. ", error);
-      });
-  };
-
-  return (
-    <div className="login-btn-outer">
-      <button className="login-btn" onClick={handleLogin}>
-        로그인
-      </button>
-    </div>
-  );
+    return (
+        <div className="login-btn-outer">
+            <button className="login-btn" onClick={handleLogin}>
+                로그인
+            </button>
+        </div>
+    );
 };
 
 const Login = () => {
-  const [idValue, setIdValue] = useState("");
-  const [pwValue, setPwValue] = useState("");
+    const [idValue, setIdValue] = useState('');
+    const [pwValue, setPwValue] = useState('');
 
-  return (
-    <div className="Login">
-      <div className="input-table-outer">
-        <form>
-          <TextBlock />
-          <br />
-          <br />
-          <InputTable
-            handleIdChange={handleIdChange}
-            handlePwChange={handlePwChange}
-          />
-          <br />
-          <LoginBtn idValue={idValue} pwValue={pwValue} />
-        </form>
-      </div>
-    </div>
-  );
+    const handleIdChange = (event) => {
+        setIdValue(event.target.value);
+    };
+
+    const handlePwChange = (event) => {
+        setPwValue(event.target.value);
+    };
+    return (
+        <div className="Login">
+            <div className="input-table-outer">
+                <form>
+                    <TextBlock />
+                    <br />
+                    <br />
+                    <InputTable handleIdChange={handleIdChange} handlePwChange={handlePwChange} />
+                    <br />
+                    <LoginBtn idValue={idValue} pwValue={pwValue} />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
