7a2386432ceb57d04f30d9289bc06c1048929cfb
DingX2 <96682768+DingX2@users.noreply.github.com>
2023-08-24 16:16:46 +0900

    [FIX] css 수정 및 모션 추가

--- news-pages/src/pages/MainPage.js
+++ news-pages/src/pages/MainPage.js
@@ -1,23 +1,35 @@
-import Sidebar from '../components/Sidebar';
-import CategoriesResult from '../components/Contents/CategoriesResult';
-import SearchResult from '../components/Contents/SearchResult';
-import { Route, Routes, Link, useLocation } from 'react-router-dom';
+import Sidebar from "../components/Sidebar";
+import CategoriesResult from "../components/Contents/CategoriesResult";
+import SearchResult from "../components/Contents/SearchResult";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
-const header_height = '101px';
+const header_height = "101px";
 
 const MainPage = () => {
-    return (
-        <div
-            className="MainPage"
-            style={{ display: 'flex', justifyContent: 'space-between', height: `calc(100% - ${header_height})` }}
-        >
-            <Sidebar />
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.6 }}
+    >
+      <div
+        className="MainPage"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          height: `calc(100% - ${header_height})`,
+        }}
+      >
+        <Sidebar />
 
-            <Routes>
-                <Route exact path="/" element={<CategoriesResult />} />
-            </Routes>
-        </div>
-    );
+        <Routes>
+          <Route exact path="/" element={<CategoriesResult />} />
+        </Routes>
+      </div>
+    </motion.div>
+  );
 };
 
 export default MainPage;
