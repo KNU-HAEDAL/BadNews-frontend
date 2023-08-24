1387508ffb6b264ed4178755a1c44e7db42a0b49
DingX2 <pourding@naver.com>
2023-08-24 20:18:02 +0900

    [FEAT] 서버와 통신 확인 1,1 넣으면 true 반환

--- news-pages/src/components/Header.js
+++ news-pages/src/components/Header.js
@@ -1,75 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { Login } from '../pages/Login';
-import './Header.css';
-import logo from '../logo.png';
-import search from '../search.png';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Login } from "../pages/Login";
+import "./Header.css";
+import logo from "../logo.png";
+import search from "../search.png";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SearchBox = () => {
-    return (
-        <div className="search-container">
-            <input id="search-box" type="text" placeholder="검색어를 입력하세요" img></input>
-            <Link to="/">
-                <img src={search} alt="돋보기" id="search-icon" />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="search-container">
+      <input
+        id="search-box"
+        type="text"
+        placeholder="검색어를 입력하세요"
+        img
+      ></input>
+      <Link to="/">
+        <img src={search} alt="돋보기" id="search-icon" />
+      </Link>
+    </div>
+  );
 };
 
 const Header = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        // 서버에 로그인 상태를 확인하는 요청을 보냅니다.
-        axios
-            .get('http://localhost:8080/checkLoginStatus')
-            .then((response) => {
-                // 서버에서 로그인 상태를 확인한 후, 그 결과를 상태로 설정합니다.
-                setIsLoggedIn(response.data.isLoggedIn);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
+  //   useEffect(() => {
+  //     // 서버에 로그인 상태를 확인하는 요청을 보냅니다.
+  //     axios
+  //       .post("http://13.124.161.27:8080/login", {
+  //         id: "1", // 사용자가 입력한 ID
+  //         password: "1", // 사용자가 입력한 Password
+  //       })
+  //       .then((response) => {
+  //         // 서버에서 로그인 상태를 확인한 후, 그 결과를 상태로 설정합니다.
+  //         console.log("response");
+  //         console.log(response.data.result);
+  //         setIsLoggedIn(response.data.result);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error:", error);
+  //       });
+  //   }, []);
 
-    const handleLogin = () => {
-        // if return(idValue) == true:
-        setIsLoggedIn(true);
-    };
+  const handleLogin = () => {
+    // if return(idValue) === true:
+    setIsLoggedIn(true);
+  };
 
-    const handleLogout = () => {
-        setIsLoggedIn(false);
-    };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
-    return (
-        <div className="Header">
-            <Link to="/" className="title-container">
-                <img src={logo} alt="Bad News logo" id="logo" />
-                <strong className="header-title">Bad News</strong>
+  return (
+    <div className="Header">
+      <Link to="/" className="title-container">
+        <img src={logo} alt="Bad News logo" id="logo" />
+        <strong className="header-title">Bad News</strong>
+      </Link>
+
+      <div className="search-login-container">
+        <SearchBox />
+
+        <div className="header-menu-container">
+          {isLoggedIn ? (
+            <Link to="/login" className="header-menu" onClick={handleLogout}>
+              로그인
             </Link>
-
-            <div className="search-login-container">
-                <SearchBox />
-
-                <div className="header-menu-container">
-                    {isLoggedIn ? (
-                        <Link to="/login" className="header-menu" onClick={handleLogout}>
-                            로그인
-                        </Link>
-                    ) : (
-                        <Link to="/" className="header-menu" onClick={handleLogin}>
-                            로그아웃
-                        </Link>
-                    )}
-                    <div className="header-menu-bar">|&nbsp;</div>
-                    <Link to="/mypage" className="header-menu">
-                        마이페이지
-                    </Link>
-                </div>
-            </div>
+          ) : (
+            <Link to="/" className="header-menu" onClick={handleLogin}>
+              로그아웃
+            </Link>
+          )}
+          <div className="header-menu-bar">|&nbsp;</div>
+          <Link to="/mypage" className="header-menu">
+            마이페이지
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- news-pages/src/pages/Login.js
+++ news-pages/src/pages/Login.js
@@ -1,116 +1,196 @@
-import './Login.css';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import "./Login.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
-const TextBlock = () => {
-    return (
-        <div style={{ paddingLeft: '5px' }}>
-            <strong className="login-title">Log In</strong>
-            <div className="txt">로그인 후 Bad News를 더욱 편리하게 사용해보세요!</div>
-        </div>
-    );
-};
+const InputTable = ({ handleIdChange, handlePwChange }) => {
+  const [idValue, setIdValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
 
-const InputTable = () => {
-    return (
-        <div className="input-table-container">
-            <table className="input-table">
-                <tr>
-                    {/* <th><label className="txt">ID</label></th> */}
-                    <td>
-                        <input id="id" type="text" placeholder="User name"></input>
-                    </td>
-                </tr>
-                <tr>
-                    {/* <th><label className="txt">PW</label></th> */}
-                    <td>
-                        <input id="pw" type="password" placeholder="Password"></input>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    );
-};
-
-const LoginBtn = () => {
-    const navigate = useNavigate();
-
-    const saveAndSend = () => {
-        //event.preventDefault();
-
-        // 변수에 인풋값 저장
-        const idValue = document.getElementById('id').value;
-        const pwValue = document.getElementById('pw').value;
-        const data = {
-            id: idValue,
-            pw: pwValue,
-        };
-
-        // 서버로 회원가입 요청
-        axios
-            .post('http://localhost:8080/signup', {
-                id: idValue,
-                password: pwValue,
-            })
-            //성공 시 then 실행
-            .then(function (response) {
-                console.log(response.data);
-            })
-            //실패 시 catch 실행
-            .catch(function (error) {
-                console.log('Error: 회원 가입 실패. 다시 시도하세요. ', error);
-            });
-
-        // 서버로 로그인 요청
-        fetch('http://localhost:8080/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result == true) {
-                    alert(`${idValue}님 환영합니다`);
-                    navigate('/'); // root로 이동
-                } else {
-                    alert('로그인 실패');
-                }
-            })
-            .catch((error) => {
-                console.error('Error: User 정보가 없습니다. ', error);
-            });
-
-        // alert(`${idValue}님 환영합니다`);
-        // navigate("/"); // root로 이동
+  const handleLogin = () => {
+    const datas = {
+      id: idValue,
+      pw: pwValue,
     };
 
-    return (
-        <div className="login-btn-outer">
-            <button className="login-btn" onClick={saveAndSend}>
-                로그인
-                {/* <Link to="/" className="styled-link">로그인</Link> */}
-            </button>
-        </div>
-    );
+    const handleIdChange = (event) => {
+      setIdValue(event.target.value);
+    };
+
+    const handlePwChange = (event) => {
+      setPwValue(event.target.value);
+    };
+  };
+  
+  return (
+    <div className="input-table-container">
+      <table className="input-table">
+        <tr>
+          {/* <th><label className="txt">ID</label></th> */}
+          <td>
+            <input
+              id="id"
+              type="text"
+              placeholder="User name"
+              onChange={handleIdChange}
+            ></input>
+          </td>
+        </tr>
+        <tr>
+          {/* <th><label className="txt">PW</label></th> */}
+          <td>
+            <input
+              id="pw"
+              type="password"
+              placeholder="Password"
+              onChange={handlePwChange}
+            ></input>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+const TextBlock = () => {
+  return (
+    <div style={{ paddingLeft: "5px" }}>
+      <strong className="login-title">Log In</strong>
+      <div className="txt">
+        로그인 후 Bad News를 더욱 편리하게 사용해보세요!
+      </div>
+    </div>
+  );
+};
+
+const LoginBtn = ({ idValue, pwValue }) => {
+  const handleLogin = () => {
+    const datas = {
+      id: idValue,
+      pw: pwValue,
+    };
+
+    axios
+      .post("http://13.124.161.27:8080/login", datas, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => response.data)
+      .then((result) => {
+        if (result === true) {
+          alert(`${idValue}님 환영합니다`);
+          // 여기에서 로그인 성공 시의 동작을 정의합니다.
+        } else {
+          alert("로그인 실패");
+        }
+      })
+      .catch((error) => {
+        console.error("Error: User 정보가 없습니다. ", error);
+      });
+  };
+
+  return (
+    <div className="login-btn-outer">
+      <button className="login-btn" onClick={handleLogin}>
+        로그인
+      </button>
+    </div>
+  );
 };
 
 const Login = () => {
-    return (
-        <div className="Login">
-            <div className="input-table-outer">
-                <form>
-                    <TextBlock />
-                    <br />
-                    <br />
-                    <InputTable />
-                    <br />
-                    <LoginBtn />
-                </form>
-            </div>
-        </div>
-    );
+  const [idValue, setIdValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
+
+  return (
+    <div className="Login">
+      <div className="input-table-outer">
+        <form>
+          <TextBlock />
+          <br />
+          <br />
+          <InputTable
+            handleIdChange={handleIdChange}
+            handlePwChange={handlePwChange}
+          />
+          <br />
+          <LoginBtn idValue={idValue} pwValue={pwValue} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
+
+// const LoginBtn = () => {
+//   const [idValue, setIdValue] = useState("");
+//   const [pwValue, setPwValue] = useState("");
+
+//   const datas = {
+//     id: idValue,
+//     pw: pwValue,
+//   };
+
+//   const handleIdChange = (event) => {
+//     setIdValue(event.target.value);
+//     console.log(event.target.value);
+//   };
+
+//   const handlePwChange = (event) => {
+//     setPwValue(event.target.value);
+//     console.log(event.target.value);
+//   };
+
+//   const navigate = useNavigate();
+
+//   const handleLogin = () => {
+//     axios
+//       .post("http://13.124.161.27:8080/login", datas, {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       })
+//       .then((response) => response.data)
+//       .then((result) => {
+//         if (result === true) {
+//           alert(`${idValue}님 환영합니다`);
+//           // navigate("/")로의 이동은 해당 코드 컨텍스트에 따라 구현되어야 합니다.
+//         } else {
+//           alert("로그인 실패");
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error: User 정보가 없습니다. ", error);
+//       });
+//   };
+
+//   return (
+//     <div className="login-btn-outer">
+//       <button className="login-btn" onClick={handleLogin}>
+//         로그인
+//         {/* <Link to="/" className="styled-link">로그인</Link> */}
+//       </button>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div className="Login">
+//       <div className="input-table-outer">
+//         <form>
+//           <TextBlock />
+//           <br />
+//           <br />
+//           <InputTable />
+//           <br />
+//           <LoginBtn />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
