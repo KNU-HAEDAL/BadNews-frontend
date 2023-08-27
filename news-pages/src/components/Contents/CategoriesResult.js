6c30c3b9890434d4d2f27087f9f7490c91709722
joojjang <itchu999@naver.com>
2023-08-27 17:35:32 +0900

    [REFACT] Article.js 생성 - 기사 영역만 따로 빼기

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -1,55 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './CategoriesResult.css';
+import Article from './Article';
 import Pagination from './Pagination';
-import unmarked from '../../bookmark_unmarked.png';
-import marked from '../../bookmark_marked.png';
 import { Link } from "react-router-dom";
-
-
-const Article = ({ article, handleBookmarkClick }) => {
-  // 컨테이너 높이 동적으로 조정
-  const [containerHeight, setContainerHeight] = useState(0);
-
-  useEffect(() => {
-    const subcontainer1 = document.querySelector(".article-subcontainer-1");
-    const subcontainer1Height = subcontainer1.offsetHeight;
-    setContainerHeight(subcontainer1Height);
-  }, []);
-
-  // 날짜를 원하는 형식으로 포맷팅
-  const formattedDate = article.date.replace('T', ' ');
-  
-  return (
-    <div className="article-container">
-      <div className="article-subcontainer-1">
-        <div className="title-bookmark-container">
-          <strong className="article-title">
-            <Link to={article.url} target="_blank">{article.title}</Link>
-          </strong>
-          <div className="bookmark-container">
-            <img
-              src={article.isMarked ? marked : unmarked}
-              alt={article.isMarked ? "marked" : "unmarked"}
-              id="bookmark"
-              onClick={() => handleBookmarkClick(article)}
-            />
-          </div>
-        </div>
-
-        <div className="article-info">{article.author}&nbsp;&nbsp;|&nbsp;&nbsp;{formattedDate}</div>
-        <div className="article-url-container">
-          <Link to={article.url} target="_blank" className="article-url">기사 원문</Link>
-        </div>
-        <div className="article-keywords">{article.keywords.map((item) => `#${item} `)}</div>
-        <div className="article-summary">{article.context}</div>
-      </div>
-
-      <div className="article-img-container" >
-        <img src={article.image.path} alt="기사 이미지" style={{ height: containerHeight }}/>
-      </div>
-    </div>
-  );
-};
 
 
 const CategoriesResult = (props) => {
