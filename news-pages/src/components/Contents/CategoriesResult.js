de43b2865b7b040c177961f8c7e938f804a2849e
joojjang <itchu999@naver.com>
2023-08-30 16:56:23 +0900

    [FIX] 홈에 중복 렌더링 수정 중, 로딩 인디케이터 추가

--- news-pages/src/components/Contents/CategoriesResult.js
+++ news-pages/src/components/Contents/CategoriesResult.js
@@ -15,6 +15,7 @@ const CategoriesResult = (props) => {
     //     }))
     // );
     const [article, setArticle] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // loading state
 
     // 북마크 상태 변경하여 article 상태 저장
     const handleBookmarkClick = (clickedArticle) => {
@@ -28,31 +29,38 @@ const CategoriesResult = (props) => {
     };
 
     // 홈에 기사 렌더링 - 서버 호출하여 값 리턴
-    // 컴포넌트가 마운트되거나 props가 변경될 때마다
+    // 컴포넌트 마운트 시
     useEffect(() => {
-        const data = {
-            userId: '1234',
-            category: 'default',
-            sort: 1,
-        };
+        if (isLoading) {
+            const data = {
+                userId: '1234',
+                category: 'default',
+                sort: 1,
+            };
 
-        axios
-            .post('http://13.125.37.219:8080/article/home', data)
-            .then(function (response) {
-                console.log('home:' + response.data);
+            axios
+                .post('http://13.125.37.219:8080/article/home', data)
+                .then(function (response) {
+                    console.log('home:' + response.data);
 
-                // Update articles with the response data
-                setArticle(
-                    response.data.map((article) => ({
-                        ...article, isMarked: false,
-                    }))
-                );
-            })
-            .catch(function (error) {
-                console.log('failed to return articles to home:', error);
-            });
-            
-    }, [props.news.state]); 
+                    // Update articles with the response data
+                    setArticle(
+                        response.data.map((article) => ({
+                            ...article, isMarked: false,
+                        }))
+                    );
+
+                    setIsLoading(false); // after data fetching
+                })
+                .catch(function (error) {
+                    console.log('failed to return articles to home:', error);
+                });
+        }
+    }, []); 
+
+    if (isLoading) {
+        return <p className="loading-state">Loading data...</p>; // Render loading indicator while fetching data
+    }
 
     console.log('값이 도착했습니다.');
     console.log(props);
